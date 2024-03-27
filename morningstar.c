290c593b6863fa6936781aad207790864d0018da
thatguy4194 <thatguy4194@tutanota.com>
2024-03-27 15:32:42 -0500

    Add time Function

--- morningstar.c
+++ morningstar.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+#include <time.h>
 
 int main() {
-    printf("Hello World!");
+    printf("Hello World!\n");
+    // Start by telling the time
+    time_t rawtime;
+    struct tm *date;
+    time(&rawtime);
+    date = localtime(&rawtime);
+    printf("%s",asctime(date));
 }
