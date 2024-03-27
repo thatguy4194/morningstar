0bd8b2c7d12c7ed34f23bede0fa249494dabf2c1
thatguy4194 <thatguy4194@tutanota.com>
2024-03-27 16:16:58 -0500

    Rename time function to getTime and remove test

--- morningstar.c
+++ morningstar.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 #include <time.h>
 
-int main() {
-    char userInput[100];
-    scanf("%s", userInput);
-    printf("%s\n", userInput);
+void getTime() {
     printf("Today's Date:\n");
     // Start by telling the time
     time_t rawtime;
